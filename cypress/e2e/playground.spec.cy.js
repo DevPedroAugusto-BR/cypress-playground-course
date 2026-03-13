ef0b7e3ce1a8a233f36aed0dfdff2a466b688a25
DevPedroAugusto-BR <pedroaugusto.mendes035@gmail.com>
2026-03-12 21:05:46 -0300

    feat: add a new test case: 'create a subscribe button'!

--- cypress/e2e/playground.spec.cy.js
+++ cypress/e2e/playground.spec.cy.js
@@ -1,6 +1,20 @@
 describe('Cypress Playground', () => {
   
+  beforeEach(() => {
+    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html');
+  })
+
   it('shows a promotional banner', () => {
     //Implementação do caso de teste
+    cy.get('#promotional-banner').should('be.visible')
+  })
+
+  it('create a subscribe button', () => {
+    cy.contains('[type="submit"]', 'Subscribe').should('be.visible').click()
+
+    cy.contains(
+      '#success',
+      "You've been successfully subscribed to our newsletter."
+    ).should('be.visible')
   })
 })
